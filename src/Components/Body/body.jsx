08873d786f9b271701f304d1b8a32941245c8daa
BrandonIsReady <brandonstirling1998@gmail.com>
2022-08-03 23:08:43 +1200

    adding hover effects, and changing sizes, pointers etc.

--- src/Components/Body/body.jsx
+++ src/Components/Body/body.jsx
@@ -6,7 +6,10 @@ export default function Body() {
       <div className={style.bodyBackground}>
         <div className={style.bodyContainer}>
           <div className={style.bodyText}>
-            <b> Lorem ipsum dolor sit amet </b>
+            <b>
+              Brandon Stirling <br />
+              Full Stack Dev
+            </b>
           </div>
 
           <div className={style.bodySearchBar}>
@@ -27,21 +30,37 @@ export default function Body() {
           <div className={style.popupOne}></div>
           <div className={style.popupOneText}>
             Exploring abandoned meatworks factory.
-            <div className={style.popupHiddenText}> further testing </div>
+            <div className={style.popupHiddenText}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+              modi suscipit reiciendis amet maiores, deserunt obcaecati aliquid?
+              Ratione pariatur unde debitis dolorem, magnam qui temporibus
+              reprehenderit harum quibusdam sed accusantium.
+            </div>
           </div>
         </div>
         <div className={style.popupOneContainer}>
           <div className={style.popupTwo}></div>
           <div className={style.popupOneText}>
             Deep in the bush somewhere in rural Gisborne.
-            <div className={style.popupHiddenText}> further testing </div>
+            <br />
+            <div className={style.popupHiddenText}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+              modi suscipit reiciendis amet maiores, deserunt obcaecati aliquid?
+              Ratione pariatur unde debitis dolorem, magnam qui temporibus
+              reprehenderit harum quibusdam sed accusantium.
+            </div>
           </div>
         </div>
         <div className={style.popupOneContainer}>
           <div className={style.popupThree}></div>
           <div className={style.popupOneText}>
             Another from abandoned meatworks factory.
-            <div className={style.popupHiddenText}> further testing </div>
+            <div className={style.popupHiddenText}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+              modi suscipit reiciendis amet maiores, deserunt obcaecati aliquid?
+              Ratione pariatur unde debitis dolorem, magnam qui temporibus
+              reprehenderit harum quibusdam sed accusantium.
+            </div>
           </div>
         </div>
       </div>
