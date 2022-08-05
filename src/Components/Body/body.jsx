88630d78e4d521ad71edf2a3deb6becbbeca2dbd
BrandonIsReady <brandonstirling1998@gmail.com>
2022-08-05 12:10:36 +1200

    final touches to the css media queries

--- src/Components/Body/body.jsx
+++ src/Components/Body/body.jsx
@@ -27,10 +27,10 @@ export default function Body() {
           <div className={style.popupOneText}>
             Exploring abandoned meatworks factory.
             <div className={style.popupHiddenText}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
-              modi suscipit reiciendis amet maiores, deserunt obcaecati aliquid?
-              Ratione pariatur unde debitis dolorem, magnam qui temporibus
-              reprehenderit harum quibusdam sed accusantium.
+              This photo is from an abandoned Freezing works factory off in
+              rural Gisborne, in a small coastal town called Tokomaru Bay. The
+              factory was built in 1911 and saw a downturn in business in 1950,
+              where it eventually closed in 1952.
             </div>
           </div>
         </div>
@@ -40,10 +40,11 @@ export default function Body() {
             Deep in the bush somewhere in rural Gisborne.
             <br />
             <div className={style.popupHiddenText}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
-              modi suscipit reiciendis amet maiores, deserunt obcaecati aliquid?
-              Ratione pariatur unde debitis dolorem, magnam qui temporibus
-              reprehenderit harum quibusdam sed accusantium.
+              My friends and I hiked over an hour upstream from our camping site
+              to find a place to have a picnic. There photo shown here, was
+              after we had been in the field for a few hours, and i was deep
+              into battling hayfever. It took myself nearly 2 hours to safely
+              walk back downstream and back to the campsite.
             </div>
           </div>
         </div>
@@ -52,10 +53,10 @@ export default function Body() {
           <div className={style.popupOneText}>
             Another from abandoned meatworks factory.
             <div className={style.popupHiddenText}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
-              modi suscipit reiciendis amet maiores, deserunt obcaecati aliquid?
-              Ratione pariatur unde debitis dolorem, magnam qui temporibus
-              reprehenderit harum quibusdam sed accusantium.
+              This one was froma more roadside spot of the freezing works
+              factory that had been abandoned, what can't be seen in the photo,
+              is the huge piles of glass from people cracking bottles, or
+              smashing out windows.
             </div>
           </div>
         </div>
